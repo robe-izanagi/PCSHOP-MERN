9eb1894f0e633737ff9de507d7fbc1b91864a1f5
robe-izanagi <robertofarol10145@gmail.com>
2025-12-16 16:15:37 +0800

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,8 +5,6 @@ const authController = require('../controllers/authController');
 router.post('/admin/login', authController.adminLogin);
 
 router.post('/register', authController.registerUser);
-router.get('/verify-email', authController.verifyEmail); // ?token=...
-
 
 router.post('/google', authController.googleAuth);
 
