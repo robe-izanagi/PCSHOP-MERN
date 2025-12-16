9fb2c46d3b0b0a81ae60760fadcc606e2763d432
robe-izanagi <robertofarol10145@gmail.com>
2025-12-16 16:23:06 +0800

    commit-new

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,5 @@ app.use('/products', require('./routes/products'));
 
 app.get('/', (req, res) => res.send('PC Shop Backend (MongoDB) Running...'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 'https://pcshop-mern-backend-rpep.onrender.com';
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
