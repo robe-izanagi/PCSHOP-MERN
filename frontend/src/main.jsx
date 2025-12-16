24f53ed2311da58cdfa7c00d93469d5839ba3a20
robe-izanagi <robertofarol10145@gmail.com>
2025-12-16 16:32:06 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './pages/App.jsx';
-import './app.css'; 
+import './App.css'; 
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
